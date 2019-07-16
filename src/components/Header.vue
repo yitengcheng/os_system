e403f379f959b8773739e41728fb6e72af03092f
fanzehua <316679581@qq.com>
2019-07-16 18:34:46 +0800

    添加头部组件，添加logo图片

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="contain">
+    <div class="title">亿隆智慧物流园办公系统</div>
+    <div>
+      <el-image src="static/wuliuyuan_logo.png" class="logo" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    data () {
+        return {};
+    },
+    computed: {},
+    methods: {}
+};
+</script>
+<style lang='scss' scoped>
+.contain {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.title {
+  font-size: 28px;
+  color: #ffffff;
+}
+.logo {
+  width: 180px;
+  height: 30px;
+}
+</style>
