828d9c8c6c38ab598dfbc73e219954c5ed5afb8e
fanzehua <316679581@qq.com>
2019-07-16 18:35:56 +0800

    添加首页，添加屏蔽目录

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class>主页</div>
+  <div>
+    <div class="companyInfoContain">
+      <div class="companyItem">
+        <icon name="职员" w="150px" h="150px" />
+        <div class="infoContain">
+          <p class="infoTitle">总人数</p>
+          <p class="info">50人</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +23,31 @@ export default {
 };
 </script>
 <style lang='scss' scoped>
+.companyInfoContain {
+  display: flex;
+  height: 200px;
+  background-color: #f3f3f3;
+}
+.companyItem {
+  display: flex;
+  flex-direction: row;
+  width: 350px;
+  background-color: #fff;
+  padding: 10px;
+  margin: 20px;
+}
+.infoContain {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  padding: 20px 30px;
+}
+.infoTitle {
+  font-size: 28px;
+}
+.info {
+  font-size: 18px;
+}
 </style>
