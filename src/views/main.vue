946520d3cddf4b36d246b3a6df04432992a61422
fanzehua <316679581@qq.com>
2019-07-17 14:16:34 +0800

    完成首页展示效果

--- src/views/main.vue
+++ src/views/main.vue
@@ -8,15 +8,99 @@
           <p class="info">50人</p>
         </div>
       </div>
+      <div class="companyItem">
+        <icon name="男毕业生" w="150px" h="150px" />
+        <div class="infoContain">
+          <p class="infoTitle">研究生以上学历</p>
+          <p class="info">30人</p>
+        </div>
+      </div>
+      <div class="companyItem">
+        <icon name="老人" w="150px" h="150px" />
+        <div class="infoContain">
+          <p class="infoTitle">返聘人数</p>
+          <p class="info">10人</p>
+        </div>
+      </div>
+      <div class="companyItem">
+        <icon name="留言板" w="150px" h="150px" />
+        <div class="infoContain">
+          <p class="infoTitle">留言板</p>
+          <p class="info">10条</p>
+        </div>
+      </div>
+      <div class="companyItem">
+        <icon name="文件" w="150px" h="150px" />
+        <div class="infoContain">
+          <p class="infoTitle">公司发文</p>
+          <p class="info">20份</p>
+        </div>
+      </div>
+    </div>
+    <div class="wordCloudContain">
+      <WordCloud></WordCloud>
+      <v-chart :options="option"></v-chart>
     </div>
   </div>
 </template>
 
 <script>
+import WordCloud from '../components/wordCloud';
 export default {
-    components: {},
+    components: { WordCloud },
     data () {
-        return {};
+        return {
+            option: {
+                color: ['#3398DB'],
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        axisLabel: { interval: 0, rotate: 30 },
+                        data: [
+                            '高层',
+                            '企管中心',
+                            '招商部',
+                            '物流管理部',
+                            '电子商务部',
+                            '辅助产业部',
+                            '财务部',
+                            '行政部',
+                            '物管部',
+                            '搬运队',
+                            '外场保洁'
+                        ],
+                        axisTick: {
+                            alignWithLabel: true
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value'
+                    }
+                ],
+                series: [
+                    {
+                        name: '部门',
+                        type: 'bar',
+                        barWidth: '60%',
+                        data: [3, 1, 5, 10, 2, 2, 2, 7, 1, 3, 3]
+                    }
+                ]
+            }
+        };
     },
     computed: {},
     methods: {}
@@ -25,7 +109,10 @@ export default {
 <style lang='scss' scoped>
 .companyInfoContain {
   display: flex;
-  height: 200px;
+  height: 450px;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
   background-color: #f3f3f3;
 }
 .companyItem {
@@ -49,5 +136,13 @@ export default {
 }
 .info {
   font-size: 18px;
+}
+.wordCloudContain {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #f3f3f3;
 }
 </style>
