72619add17bf35aa5b2c6cee6a5c22151968857b
fanzehua <316679581@qq.com>
2019-07-17 15:24:26 +0800

    添加页面抬头组件

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class>
+    <h1 class="title">{{label}}</h1>
+    <el-divider></el-divider>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['label']
+};
+</script>
+<style lang='scss' scoped>
+@import "../assets/scss/baseAttribute";
+.title {
+  font-size: $text-title;
+}
+</style>
