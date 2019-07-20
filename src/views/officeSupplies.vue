9f19abe59588954f50b6cddb4bde60c9551d0ee3
fanzehua <316679581@qq.com>
2019-07-20 10:33:59 +0800

    修改办公用品管理页面

--- src/views/officeSupplies.vue
+++ src/views/officeSupplies.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="contain">
     <PageTitle label="办公用品管理" />
+    <div class="tableContain">
+      <div>
+        <el-button>办公用品入库</el-button>
+      </div>
+      <ElTable :stripe="true" :border="true" :tableKey="labels"></ElTable>
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from '../components/PageTitle';
+import ElTable from '../components/elComponent/el-table';
 export default {
-    components: { PageTitle },
+    components: { PageTitle, ElTable },
     data () {
-        return {};
+        return {
+            labels: [
+                {
+                    prop: 'category',
+                    label: '类别',
+                    width: '200'
+                },
+                {
+                    prop: 'name',
+                    label: '名称',
+                    width: '200'
+                },
+                {
+                    prop: 'number',
+                    label: '数量',
+                    width: '200'
+                },
+                {
+                    label: '操作',
+                    width: '300'
+                }
+            ]
+        };
     },
     computed: {},
     methods: {}
@@ -22,5 +51,10 @@ export default {
   flex-direction: column;
   justify-content: center;
   padding: 10px;
+}
+.tableContain {
+  display: flex;
+  flex-direction: column;
+  align-self: center;
 }
 </style>
