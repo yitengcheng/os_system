faf379a383d0ea215aa27d7b1ee7538b9efca904
fanzehua <316679581@qq.com>
2019-07-20 16:24:15 +0800

    修改接口转发地址

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
         assetsPublicPath: '/',
         proxyTable: {
             '/api': {
-                target: 'http://127.0.0.1:3000'
+                target: 'http://192.168.0.118:3000'
             }
         },
 
