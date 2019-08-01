4c73ebb44a476af1addb763f2024eef890727ff4
lihonghui <284068069@qq.com>
2019-08-01 14:49:16 +0800

    通知消息页面

--- src/views/informationIssue.vue
+++ src/views/informationIssue.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="contain">
     <PageTitle label="信息发布" />
-    <el-form class="formContain" :model="form" label-width="110px" label-position="right">
+    <el-form
+      class="formContain"
+      ref="form"
+      :model="form"
+      label-width="110px"
+      label-position="right"
+      :rules="rules"
+    >
       <FormInput label="标题" :form="form" value="title" @onChange="onChange" />
       <FormInput
         type="textarea"
-        maxlength="30"
+        maxlength="200"
         :showWordLimit="true"
         :autosize="{ minRows: 6, maxRows: 6 }"
-        label="描述"
+        label="正文"
         :form="form"
         value="desc"
         resize="none"
         @onChange="onChange"
       />
-      <el-button class="confirmBtn" type="primary">确认发布</el-button>
+      <el-button class="confirmBtn" type="primary" @click="sent">确认发布</el-button>
     </el-form>
   </div>
 </template>
@@ -22,20 +29,55 @@
 <script>
 import PageTitle from '../components/PageTitle';
 import FormInput from '../components/form/formInput';
+import { mapState } from 'vuex';
 export default {
     components: { PageTitle, FormInput },
     data () {
         return {
+            rules: {
+                title: { required: true, message: '请填写标题', trigger: 'blur' },
+                desc: { required: true, message: '请填写正文', trigger: 'blur' }
+            },
             form: {
                 title: '',
                 desc: ''
             }
         };
     },
-    computed: {},
+    computed: {
+        ...mapState({
+            user: state => state.user.user
+        })
+    },
     methods: {
         onChange (value, formType) {
             this.form[formType] = value;
+        },
+        sent () {
+            if (!this.user._id) {
+                this.$alert('请先登录');
+                return;
+            }
+            this.$refs.form.validate(valid => {
+                if (valid) {
+                    this.$http
+                        .post('/api/informationIssue', {
+                            information: this.form,
+                            userId: this.user._id
+                        })
+                        .then(res => {
+                            let { success, msg } = res;
+                            if (success) {
+                                this.$refs.form.resetFields();
+                                this.$alert('发布成功');
+                            } else {
+                                this.$alert(msg);
+                            }
+                        });
+                } else {
+                    this.$alert('信息填写有误，请重新填写');
+                }
+            });
         }
     }
 };
